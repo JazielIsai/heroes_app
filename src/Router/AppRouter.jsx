feaeb59a783e31d92309746dc8a4a3ee414beeae
JazielIsai <code.isai.dev@gmail.com>
2022-07-28 23:36:09 -0500

    delete all the variables without be used

--- src/Router/AppRouter.jsx
+++ src/Router/AppRouter.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 
-import {Navbar} from '../ui';
 import { LoginPage } from '../auth';
 import { HeroesRoutes } from '../heroes';
 import { PrivateRoute } from './PrivateRoute';
